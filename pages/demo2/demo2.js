d5c517ec3723a68adabd7ea3a1c86d30d9185da0
Amfc <longloved@yeah.net>
2021-04-14 14:25:34 +0800

    update

---
+++ pages/demo2/demo2.js
@@ -0,0 +1,70 @@
+// pages/demo2/demo2.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    title:"2021微信小程序开发",
+    content:"不管遇到什么困难，勇敢地去面对它，干就完了，奥里给！！！",
+    listArr:["平面设计","UI设计","web前端"],
+    obj:{name:"王昊磊",age:20,sex:"男"},
+    day:3
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/demo3/demo3.js
@@ -0,0 +1,71 @@
+// pages/demo3/demo3.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    listArr:["平面设计","室内设计","UI设计","影视后期","web前端"],
+    listObj:[
+      {title:"新视觉实训1",time:"2021-4-12"},
+      {title:"基地动态1",time:"2021-3-29"},
+      {title:"学生作品展示",time:"2020-9-23"}
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
